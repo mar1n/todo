1a56341c5b5c049fb9ac9ff9c29fdd89d917c587
mar1n <30738216+mar1n@users.noreply.github.com>
2020-03-24 00:38:44 +0000

    list of todo on fornt and count checkes

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App extends React.Component {
     super(props);
     this.state = {
       userName: "Adam",
-      todoItems: [{action: "Buy Flowers", done: false},
-                  {action: "Get Shoes", done: false},
-                  {action: "Collect Tickets", done: true},
-                  {action: "Call Joe", done: false}],
+      todoItems: [{ action: "Buy Flowers", done: false },
+      { action: "Get Shoes", done: false },
+      { action: "Collect Tickets", done: true },
+      { action: "Call Joe", done: false }],
       newItemText: ""
     }
   }
@@ -21,34 +21,66 @@ class App extends React.Component {
 
   changeStateData = () => {
     this.setState({
-      userName: this.state.userName === "Adam" ? "Bob" : "Adam"
+      userName: this.state.userName === "Adam"
+        ? "Bob"
+        : "Adam"
     })
   }
 
   createNewTodo = () => {
-    if(!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
+    if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
       this.setState({
         todoItems: [...this.state.todoItems, { action: this.state.newItemText, done: false }],
         newItemText: ""
       });
     }
   }
-  render = () => 
-      <div>
-        <h4 className="bg-primary text-white text-center p-2">
-          {this.state.userName}'s To Do list
-          ({ this.state.todoItems.filter(t => !t.done).length} items to do)
+
+  toggleTodo = (todo) => this.setState({
+    todoItems: this.state.todoItems.map(
+      item => item.action === todo.action
+        ? { ...item, done: !item.done }
+        : item
+    )
+  });
+
+  todoTableRows = () => this.state.todoItems.map(item =>
+    <tr key={item.action}>
+      <td>{item.action}</td>
+      <td>
+        <input
+          type="checkbox"
+          checked={item.done}
+          onChange={() => this.toggleTodo(item)}
+        />
+      </td>
+    </tr>
+  );
+  render = () =>
+    <div>
+      <h4 className="bg-primary text-white text-center p-2">
+        {this.state.userName}'s To Do list
+        ({this.state.todoItems.filter(t => !t.done).length} items to do)
       </h4>
-        <div className="my-1">
-          <input className="form-control"
-                value={ this.state.newItemText }
-                onChange={ this.updateNewTextValue } />
-            <button className="btn btn-primary mt-1"
-                onClick= { this.createNewTodo }>
-                  Add
-                </button>
-        </div>
+      <div className="my-1">
+        <input
+          className="form-control"
+          value={this.state.newItemText}
+          onChange={this.updateNewTextValue}
+        />
+        <button className="btn btn-primary mt-1"
+          onClick={this.createNewTodo}
+        >
+          Add
+        </button>
       </div>
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr><th>Description</th><th>Done</th></tr>
+        </thead>
+        <tbody>{this.todoTableRows() }</tbody>
+      </table>
+    </div>
 }
 
 export default App;
